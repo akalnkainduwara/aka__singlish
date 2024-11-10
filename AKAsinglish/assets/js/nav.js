9364d1ff8b03f65c9717838ca65ae78ca56e6cd3
akalnkainduwara <akalankainduwara123@outlook.com>
2024-11-10 13:50:45 +0530

    Add files via upload

---
+++ AKAsinglish/assets/js/nav.js
@@ -0,0 +1,15 @@
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '10px';
+  }
+}
+
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+   location.replace("http://sinhalaunicode.helalk.com/");
+   Image("assets/images/Screenshot 2024-11-07 182228.png");
+}
+
+  
